1befe0c402039d4be70c5201108b0f8344c52ca6
Zhongkai Kevin Hu <126332675+KevinH1025@users.noreply.github.com>
2025-03-20 00:10:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // Ensure correct deployment base
   plugins: [react()],
-  base: '/',
+  resolve: {
+    extensions: [".js", ".jsx"], // Ensures proper file resolution
+  },
 });
